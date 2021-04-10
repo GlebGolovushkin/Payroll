24c6ff2c486cc86820c0423e6b8ada3f9ec75a13
GlebGolovushkin <shadaw_2010@mail.ru>
2021-04-10 22:33:28 +0300

    Fixed namings

--- Payroll.Tests/Core/EmployeeRepositoryTest.cs
+++ Payroll.Tests/Core/EmployeeRepositoryTest.cs
@@ -23,35 +23,35 @@ namespace Payroll.Tests.Core
         public void CompanySalaryCalculatorVisitorTestCalculation(int yearsOfWork, double expected)
         {
             // Arrange// Creating employees with date of employment and base salary without any bonuses.
-            var s = new Sales(DateTime.Now.AddYears(-11), 10000);
-            var m1 = new Manager(DateTime.Now.AddYears(-10), 8500);
-            var e1 = new Employee(DateTime.Now.AddYears(-8), 7300);
-            var s1 = new Sales(DateTime.Now.AddYears(-5), 6000);
-            var e2 = new Employee(DateTime.Now.AddYears(-2), 4000);
-            var m2 = new Manager(DateTime.Now.AddYears(-2), 4000);
-            var m3 = new Manager(DateTime.Now.AddYears(-1), 3700);
-            var e3 = new Employee(DateTime.Now.AddYears(-2), 4000);
-            var e4 = new Employee(DateTime.Now.AddYears(-3), 4500);
-            var m4 = new Manager(DateTime.Now.AddYears(-13), 9500);
-            var m5 = new Manager(DateTime.Now.AddYears(-7), 7300);
-            var e5 = new Employee(DateTime.Now.AddYears(-2), 3000);
-            var e6 = new Employee(DateTime.Now.AddYears(-1), 2000);
-            var m6 = new Manager(DateTime.Now, 1800);
+            var sales = new Sales(DateTime.Now.AddYears(-11), 10000);
+            var manager1 = new Manager(DateTime.Now.AddYears(-10), 8500);
+            var employee1 = new Employee(DateTime.Now.AddYears(-8), 7300);
+            var sales1 = new Sales(DateTime.Now.AddYears(-5), 6000);
+            var employee2 = new Employee(DateTime.Now.AddYears(-2), 4000);
+            var manager2 = new Manager(DateTime.Now.AddYears(-2), 4000);
+            var manager3 = new Manager(DateTime.Now.AddYears(-1), 3700);
+            var employee3 = new Employee(DateTime.Now.AddYears(-2), 4000);
+            var employee4 = new Employee(DateTime.Now.AddYears(-3), 4500);
+            var manager4 = new Manager(DateTime.Now.AddYears(-13), 9500);
+            var manager5 = new Manager(DateTime.Now.AddYears(-7), 7300);
+            var employee5 = new Employee(DateTime.Now.AddYears(-2), 3000);
+            var employee6 = new Employee(DateTime.Now.AddYears(-1), 2000);
+            var manager6 = new Manager(DateTime.Now, 1800);
 
             // Linking all employees.
-            s.AddSubordinates(m1, e1, s1);
-            m1.AddSubordinates(e2, m2);
-            s1.AddSubordinates(m3);
-            m3.AddSubordinates(e3);
-            m3.AddSubordinates(e4);
-            m4.AddSubordinates(m5, e5);
-            m5.AddSubordinates(e6, m6);
+            sales.AddSubordinates(manager1, employee1, sales1);
+            manager1.AddSubordinates(employee2, manager2);
+            sales1.AddSubordinates(manager3);
+            manager3.AddSubordinates(employee3);
+            manager3.AddSubordinates(employee4);
+            manager4.AddSubordinates(manager5, employee5);
+            manager5.AddSubordinates(employee6, manager6);
 
             // Creating employee repository.
             var repository = new EmployeeRepository();
 
             // Linking employee repository.
-            repository.AddEmployees(s, s1, m1, m2, m3, m4, m5, m6, e1, e2, e3, e4, e5, e6);
+            repository.AddEmployees(sales, sales1, manager1, manager2, manager3, manager4, manager5, manager6, employee1, employee2, employee3, employee4, employee5, employee6);
 
             // Act
             // Getting sum of all salaries in company.
